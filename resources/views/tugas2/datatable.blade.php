49fcaeb300d890afa9d2b94d2e0d9257e1d2de2e
Rita <irensyarahma@gmail.com>
2023-11-22 09:59:00 +0700

    push2

--- resources/views/tugas2/datatable.blade.php
+++ resources/views/tugas2/datatable.blade.php
@@ -444,4 +444,12 @@
             </div>
         </div>
     </div>
+
+    <script src="../../plugins/datatables/jquery.dataTables.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<script>
+$(function () {
+$("#example1").DataTable();
+});
+</script>
 @endsection
